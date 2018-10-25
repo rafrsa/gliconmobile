1caa80d41fc1376f221430281f9cb7fa26de7124
Rafael Frecceiro Santos <rafa.frecceiro@gmail.com>
2018-10-25 16:13:55 -0300

    Primeiro commit

---
+++ platforms/android/app/build/generated/source/buildConfig/debug/com/glicon/gliconapp2/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.glicon.gliconapp2;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.glicon.gliconapp2";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 201010;
+  public static final String VERSION_NAME = "1.0.4";
+}
